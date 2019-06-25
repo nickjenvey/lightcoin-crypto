25daeac2d4c56c0568fe534fafad650472029562
Nick <nbjenvey@gmail.com>
2019-06-25 20:13:19 +0000

    Completed exercise

--- index.js
+++ index.js
@@ -1,29 +1,79 @@
-let balance = 500.00;
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
+  }
 
-class Withdrawal {
+  get balance() {
+    let balance = 0;
+    for (const i of this.transactions) {
+      balance += i.value;
+    }
+    return balance;
+  }
 
-  constructor(amount) {
+  addTransaction(transactions) {
+    this.transactions.push(transactions);
+  }
+}
+
+class Transaction {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    if(!this.isAllowed()) {
+      return false;
+    }
+    this.time = new Date();
+    this.account.addTransaction(this);
+    return true;
   }
-
 }
 
+class Deposit extends Transaction {
+  get value() {
+    return this.amount;
+  }
 
+  isAllowed() {
+    return true;
+  }
+}
 
+class Withdrawal extends Transaction {
+  get value() {
+    return -this.amount;
+  }
+
+  isAllowed() {
+    return (this.account.balance - this.amount >= 0);
+  }
+}
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
+const myAccount = new Account("Neezy Boscreezy");
 
-t1 = new Withdrawal(50.25);
+console.log("Beginning Balance:", myAccount.balance);
+
+t1 = new Deposit(120, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
+console.log("Current Balance:", myAccount.balance);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(60, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
+console.log("Current Balance:", myAccount.balance);
 
-console.log('Balance:', balance);
+t3 = new Withdrawal(60, myAccount);
+t3.commit();
+console.log("Current Balance:", myAccount.balance);
+
+t4 = new Withdrawal(60, myAccount);
+t4.commit();
+console.log("Trying to withdrawal another 60 should not work");
+console.log("Current Balance:", myAccount.balance);
+
+console.log("Final Balance:", myAccount.balance);
